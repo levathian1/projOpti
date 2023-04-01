ed5c9c499b82ec14c7811314d903bdba18ab3be3
Nacera Elarbi <randomynot02@gmail.com>
2023-04-01 15:17:59 +0200

    switched to matrix style implementation

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import numpy as np
 import random
 
 matrix = pd.read_csv("freqBigrammes.txt", sep = "\t")
 
-keys = list("abcdefghijklmnopqrstuvwxyz")
+key_array = np.array([["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t"], ["u", "v", "w", "x", "y", "z", " ", " ", " ", " "],[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]])
+
+perm_key_arr = np.array([["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t"], ["u", "v", "w", "x", "y", "z", " ", " ", " ", " "],[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]])
+
 perm_key = list("abcdefghijklmnopqrstuvwxyz")
 
-current_layout = keys
+current_layout = key_array
+current_pair = list()
 
 tabul = list()
 tabu = 10
-max_iter = 100
+max_iter = 5
+
+def get_bigram_val(x, y):
+    return matrix.loc[x][y]
 
 def distance_manhattan(p1x, p1y, p2x, p2y):
     return abs(p1x-p2x)+abs(p1y-p2y)
@@ -19,15 +27,16 @@ def distance_manhattan_on_kb(kb, perm=perm_key):
     distance = 0
     lrow, lcol = 0, 0
     for letter in perm_key:
-        row, col = kb.index(letter)//10, kb.index(letter)%10
-        distance = distance_manhattan(row, lrow, col, lcol)
+        row, col = np.where(kb == letter)[0], np.where(kb == letter)[1]
+        print(row, col, letter, np.where(kb == letter)[0])
+        distance = distance + distance_manhattan(row, lrow, col, lcol)
         lrow, lcol = row, col
     return distance
 
 def swap_keys(kb):
     new_layout = kb
-    i, j = random.sample(range(len(kb)), 2)
-    new_layout[i], new_layout[j] = new_layout[j], new_layout[i]
+    ix, iy,  jx, jy = random.sample(range(len(kb)), 4)
+    new_layout[ix][iy], new_layout[jx][jy] = new_layout[jx][jy], new_layout[ix][iy]
     return new_layout
 
 best_kb = current_layout
@@ -46,4 +55,5 @@ for i in range (0, max_iter):
 
 #print(matrix)
 print(perm_key)
+print(perm_key_arr)
 print(best_kb)
