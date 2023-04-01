0f3f9c9db60e046357aac19567ec1a6f1daf25ba
Nacera Elarbi <randomynot02@gmail.com>
2023-04-01 15:27:25 +0200

    added bigram consideration to next neighbour

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 
-matrix = pd.read_csv("freqBigrammes.txt", sep = "\t")
+matrix = pd.read_csv("freqBigrammes.txt", sep = "\t", header=0, index_col=0)
 
 key_array = np.array([["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t"], ["u", "v", "w", "x", "y", "z", " ", " ", " ", " "],[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]])
 
@@ -19,6 +19,18 @@ max_iter = 5
 
 def get_bigram_val(x, y):
     return matrix.loc[x][y]
+
+def get_kb_bigram(kb, dist):
+    for i in range(0, 4):
+        for j in range(0, 9):
+            if(kb[i][j] != " " and kb[i][j+1] != " "):
+                key = kb[i][j]
+                key = key.upper() + "_"
+                next = kb[i][j+1]
+                next ="_" + next.upper()
+                dist = dist * get_bigram_val(key, next)
+    return dist
+
 
 def distance_manhattan(p1x, p1y, p2x, p2y):
     return abs(p1x-p2x)+abs(p1y-p2y)
@@ -40,12 +52,12 @@ def swap_keys(kb):
     return new_layout
 
 best_kb = current_layout
-best_d = distance_manhattan_on_kb(best_kb)
+best_d = get_kb_bigram(best_kb, distance_manhattan_on_kb(best_kb))
 
 for i in range (0, max_iter):
     new_layout = swap_keys(current_layout)
     if tuple(new_layout) not in tabul:
-        new_distance = distance_manhattan_on_kb(new_layout)
+        new_distance = get_kb_bigram(best_kb, distance_manhattan_on_kb(best_kb))
         if new_distance < best_d:
             current_layout = new_layout
             best_d = new_distance
